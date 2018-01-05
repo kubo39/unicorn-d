aa5152758784ff39a1d298a0f45b3d18ece859e9
Hiroki Noda <kubo39@gmail.com>
2018-01-06 07:30:10 +0900

    Add more constants

--- source/unicorn/constants.d
+++ source/unicorn/constants.d
@@ -47,6 +47,63 @@ enum Status
     HOOK_EXIST, // hook for this event already existed
 }
 
+enum MemType
+{
+    READ = 16, // Memory is read from
+    WRITE, // Memory is written to
+    FETCH, // Memory is fetched
+    READ_UNMAPPED, // Unmapped memory is read from
+    WRITE_UNMAPPED, // Unmapped memory is written to
+    MEM_FETCH_UNMAPPED, // Unmapped memory is fetched
+    WRITE_PROT, // Write to write protected, but mapped, memory
+    READ_PROT, // Read from read protected, but mapped, memory
+    FETCH_PROT, // Fetch from non-executable, but mapped, memory
+}
+
+enum HookType
+{
+    INTR = 1 << 0, // Hook all interrupt/syscall events
+    INSN = 1 << 1, // Hook a particular instruction
+    CODE = 1 << 2, // Hook a range of code
+    BLOCK = 1 << 3, // Hook basic blocks
+    MEM_READ_UNMAPPED = 1 << 4, // Hook for memory read on unmapped memory
+    MEM_WRITE_UNMAPPED = 1 << 5, // Hook for invalid memory write events
+    MEM_FETCH_UNMAPPED = 1 << 6, // Hook for invalid memory fetch for execution events
+    MEM_READ_PROT = 1 << 7, // Hook for memory read on read-protected memory
+    MEM_WRITE_PROT = 1 << 8, // Hook for memory write on write-protected memory
+    MEM_FETCH_PROT = 1 << 9, // Hook for memory fetch on non-executable memory
+    MEM_READ = 1 << 10, // Hook memory read events.
+    MEM_WRITE = 1 << 11, // Hook memory write events.
+    MEM_FETCH = 1 << 12, // Hook memory fetch for execution events
+}
+
+enum CodeHookType
+{
+    CODE = 1 << 2, // Hook a range of code
+    BLOCK = 1 << 3, // Hook basic blocks
+}
+
+enum MemHookType
+{
+    MEM_READ_UNMAPPED = 1 << 4, // Hook for memory read on unmapped memory
+    MEM_WRITE_UNMAPPED = 1 << 5, // Hook for invalid memory write events
+    MEM_FETCH_UNMAPPED = 1 << 6, // Hook for invalid memory fetch for execution events
+    MEM_READ_PROT = 1 << 7, // Hook for memory read on read-protected memory
+    MEM_WRITE_PROT = 1 << 8, // Hook for memory write on write-protected memory
+    MEM_FETCH_PROT = 1 << 9, // Hook for memory fetch on non-executable memory
+    MEM_READ = 1 << 10, // Hook memory read events.
+    MEM_WRITE = 1 << 11, // Hook memory write events.
+    MEM_FETCH = 1 << 12, // Hook memory fetch for execution events
+    MEM_UNMAPPED = 0b111 << 4, // hook type for all events of unmapped memory access
+    MEM_PROT = 0b111 << 7, // hook type for all events of illegal protected memory access
+    MEM_READ_INVALID = (1 << 4) | (1 << 7), /* Hook type for all events of illegal read memory access */
+    MEM_WRITE_INVALID = (1 << 5) | (1 << 8), /* Hook type for all events of illegal write memory access/ */
+    MEM_FETCH_INVALID = (1 << 6) | (1 << 9), /* Hook type for all events of illegal fetch memory access */
+    MEM_INVALID = (0b111111 << 4), // Hook type for all events of illegal memory access
+    MEM_VALID = (0b111 << 10), // Hook type for all events of valid memory access
+    MEM_ALL = 0b111111111 << 4, // Hook type for all events.
+}
+
 enum Query
 {
     MODE = 1,
