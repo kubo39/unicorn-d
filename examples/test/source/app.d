aca0942e6afaf715c9e2ea09318357b1e1bc7cfb
Hiroki Noda <kubo39@gmail.com>
2018-01-09 05:30:17 +0900

    Ooops!

--- examples/test/source/app.d
+++ examples/test/source/app.d
@@ -5,7 +5,7 @@ void main()
 {
     auto ver = unicornVersion();
     writefln("unicorn version: %d.%d", ver[0], ver[1]);
-    writeln("IsARM supported?: ", archSupported(Arch.X86));
+    writeln("IsARM supported?: ", archSupported(Arch.ARM));
 
     auto emu = new CpuARM(Mode.THUMB);
     auto pageSize = emu.query(Query.PAGE_SIZE);
